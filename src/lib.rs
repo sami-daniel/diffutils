add9ec28ddd6a5a8f422832aa5078fbf25570377
Sami Daniel <samidanielpersonal@gmail.com>
2025-05-06 06:31:32 -0300

    Update src/lib.rs
    Rename export to side_by_side_diff
    
    Co-authored-by: Sylvestre Ledru <sylvestre@debian.org>

--- src/lib.rs
+++ src/lib.rs
@@ -12,5 +12,5 @@ pub mod utils;
 pub use context_diff::diff as context_diff;
 pub use ed_diff::diff as ed_diff;
 pub use normal_diff::diff as normal_diff;
-pub use side_diff::diff as side_by_syde_diff;
+pub use side_diff::diff as side_by_side_diff;
 pub use unified_diff::diff as unified_diff;
