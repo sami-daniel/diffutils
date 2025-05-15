2c99ffc79fde570cfffb6789bc05309431231d86
Sami Daniel (Tsoi) <samidanielpersonal@gmail.com>
2025-05-14 22:43:24 -0300

    --

--- src/side_diff.rs
+++ src/side_diff.rs
@@ -3,6 +3,7 @@
 // For the full copyright and license information, please view the LICENSE-*
 // files that was distributed with this source code.
 
+use core::cmp::{max, min};
 use diff::Result;
 use std::{
     io::{stdout, StdoutLock, Write},
@@ -10,8 +11,8 @@ use std::{
 };
 use unicode_width::UnicodeWidthChar;
 
-const SDIFF_HALF_WIDTH: usize = 60;
-const TAB_SIZE: usize = 8;
+const GUTTER_WIDTH_MIN: isize = 3; // The MIDDLE mark size
+const WIDTH: isize = 130; // Can be overrided by -w option, just an temporary solution
 
 fn format_tabs_and_spaces(
     from: usize,
@@ -61,7 +62,7 @@ fn process_half_line(
         buf.push(b' ');
     }
 
-    //The encoding will probably be compatible with utf8, so we can take advantage
+    // The encoding will probably be compatible with utf8, so we can take advantage
     // of that to get the size of the columns and iterate without breaking the encoding of anything.
     // It seems like a good trade, since there is still a fallback in case it is not utf8.
     if is_utf8 {
@@ -151,6 +152,8 @@ fn push_output(
     symbol: u8,
     left_ln_buffer: &mut Vec<u8>,
     right_ln_buffer: &mut Vec<u8>,
+    sdiff_half_width: usize,
+    tab_size: usize,
 ) -> std::io::Result<()> {
     const EXPANDED: bool = false; // should come from the flag -t,
 
@@ -159,18 +162,18 @@ fn push_output(
 
     process_half_line(
         left_ln,
-        SDIFF_HALF_WIDTH + 1,
+        sdiff_half_width,
         EXPANDED,
-        TAB_SIZE,
+        tab_size,
         false,
         left_ln_buffer,
     )
     .unwrap();
     process_half_line(
         right_ln,
-        SDIFF_HALF_WIDTH + 1,
+        sdiff_half_width,
         EXPANDED,
-        TAB_SIZE,
+        tab_size,
         true,
         right_ln_buffer,
     )
@@ -193,8 +196,26 @@ pub fn diff(from_file: &[u8], to_file: &[u8]) -> Vec<u8> {
     let left_lines: Vec<&[u8]> = from_file.split(|&c| c == b'\n').collect();
     let right_lines: Vec<&[u8]> = to_file.split(|&c| c == b'\n').collect();
 
-    let mut left_ln_buf = Vec::with_capacity(SDIFF_HALF_WIDTH + 1);
-    let mut right_ln_buf = Vec::with_capacity(SDIFF_HALF_WIDTH + 1);
+    // for some reason that I could not identify, GNU diff
+    // use this calc to calculate the size of  half line
+    // based on options passed (like -w, -t, etc. ). Actually, is pretty uselles, cause we
+    // dont have any size modifiers that can alter this, however
+    // I just want to leave here the calc, since it's not clear
+    // and can make some sort of mess
+
+    let t = /* expanded_tabs ? 1 : tabsize, however, we dont have -t opt, so it will be always 8 */ 8;
+    let t_plus_g = t + GUTTER_WIDTH_MIN;
+    // The first part gets the exactly half of WIDTH rounded floor,
+    // then sum with the exactly half of t_plus_g rounded floor,
+    // then, if both WIDTH and t_plus_g are odd, add one
+    let unaligned_off = (WIDTH >> 1) + (t_plus_g >> 1) + (WIDTH & t_plus_g & 1);
+    // unaligned_off - the next of t, rounded floor, cause the sdiff_half_width + GUTTER_WIDTH_MAX
+    // has always to be an multiple of tabsize, to garantee the alignment
+    let off = unaligned_off - unaligned_off % t;
+    let sdiff_half_width = (max(0, min(off - GUTTER_WIDTH_MIN, WIDTH - off))) as usize;
+
+    let mut left_ln_buf = Vec::with_capacity(sdiff_half_width);
+    let mut right_ln_buf = Vec::with_capacity(sdiff_half_width);
 
     for result in diff::slice(&left_lines, &right_lines) {
         match result {
@@ -206,6 +227,8 @@ pub fn diff(from_file: &[u8], to_file: &[u8]) -> Vec<u8> {
                     b'<',
                     &mut left_ln_buf,
                     &mut right_ln_buf,
+                    sdiff_half_width,
+                    t as usize,
                 )
                 .unwrap();
             }
@@ -217,6 +240,8 @@ pub fn diff(from_file: &[u8], to_file: &[u8]) -> Vec<u8> {
                     b'>',
                     &mut left_ln_buf,
                     &mut right_ln_buf,
+                    sdiff_half_width,
+                    t as usize,
                 )
                 .unwrap();
             }
@@ -228,6 +253,8 @@ pub fn diff(from_file: &[u8], to_file: &[u8]) -> Vec<u8> {
                     b' ',
                     &mut left_ln_buf,
                     &mut right_ln_buf,
+                    sdiff_half_width,
+                    t as usize,
                 )
                 .unwrap();
             }
