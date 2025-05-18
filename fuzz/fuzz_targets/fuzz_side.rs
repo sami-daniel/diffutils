a9713abb5776241da0475ea5afa748793593d5d9
Sami Daniel (Tsoi) <samidanielpersonal@gmail.com>
2025-05-18 18:57:52 -0300

    --

--- fuzz/fuzz_targets/fuzz_side.rs
+++ fuzz/fuzz_targets/fuzz_side.rs
@@ -6,12 +6,23 @@ use diffutilslib::side_diff;
 
 use std::fs::File;
 use std::io::Write;
+use diffutilslib::params::Params;
 
-fuzz_target!(|x: (Vec<u8>, Vec<u8>)| {
-    let (original, new) = x;
+fuzz_target!(|x: (Vec<u8>, Vec<u8>, usize, usize, bool)| {
+    let (original, new, width, tabsize, expand) = x;
     
+    if width == 0 || tabsize == 0 {
+        return;
+    }
+    
+    let params = Params {
+        width,
+        tabsize,
+        expand_tabs: expand,
+        ..Default::default()
+    };
     let mut output_buf = vec![];
-    side_diff::diff(&original, &new, &mut output_buf);
+    side_diff::diff(&original, &new, &mut output_buf, &params);
     File::create("target/fuzz.file.original")
         .unwrap()
         .write_all(&original)
